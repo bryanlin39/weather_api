bf9ea7c2dfa4f1bcf95c093fd930fffe62697cbd
Bryan Lin and Ilene Gorski <bryan+ilene@>
2017-05-23 14:54:25 -0700

    add sunset

--- gulpfile.js
+++ gulpfile.js
@@ -75,7 +75,7 @@ gulp.task('build', ['clean'], function(){
   gulp.start('bower');
 });
 
-gulp.task('serve', function() {
+gulp.task('serve', ['build'], function() {
   browserSync.init({
     server: {
       baseDir: "./",
--- js/weather-interface.js
+++ js/weather-interface.js
@@ -2,14 +2,21 @@ var apiKey = '296a696907c2ed5c09cc9be7c62de78b';
 
 $(document).ready(function() {
   $('form').submit(function(event) {
+    $('#temperature').empty();
+    $('#sunset').empty();
     event.preventDefault()
     var city = $('#city').val();
     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=' + apiKey).then(function(response) {
       console.log(response)
       $('#humidity').text('The humidity in ' + city + ' is ' + response.main.humidity + '%');
       $('#temperature').text('The temperature in ' + city + ' is ' + response.main.temp + ' degrees');
-      $('#sunset').text('The sunset in ' + city + ' will be ' + response.main.temp + ' degrees');
-    });
+      time = new Date(response.sys.sunset * 1000);
+      var hours = time.getHours();
+      var minutes = time.getMinutes();
+      var seconds = time.getSeconds();
+      var sunsetTime = hours + ':' + minutes;
+      $('#sunset').text('The sunset in ' + city + ' will be at ' + sunsetTime);
+    })
 
     .fail(function(error) {
       $('#humidity').text(error.responseJSON.message);
